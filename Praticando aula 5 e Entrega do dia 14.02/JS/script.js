bbbe7146a33debaecea9f3ee89e54a714e96f65b
Pedro Lucas Carolino Felix <pedro.carolino.develop@gmail.com>
2023-02-16 18:10:20 -0300

    Entrega 5

---
+++ Praticando aula 5 e Entrega do dia 14.02/JS/script.js
@@ -0,0 +1,56 @@
+function verificacaoDeDados (a,b,c,d) {
+    
+    if (a <= 0 || b <= 0 || c <= 0 || d <= 0) {
+        return(a);
+    } else if  (a > 10 || b > 10 || c > 10 || d > 10) {
+        return(a);
+    }
+}
+
+function matematicaERedacaoPeso (a) {
+    b = a * 3;
+    return (b)
+}
+
+function portuguesECienciasPeso(c) {
+    d = c * 2
+    return(d)
+}
+
+function notaTotalPeso(a,b,c,d) {
+    f = (a + b + c + d)/10
+    return(f)
+}
+
+function resultado(a) {
+    
+    if(a => 7) {
+        alert("Passou, a sua nota foi " + a);
+    } else {
+        alert("Não Passou, a sua nota foi " + a);
+        b = 7 - a;
+        alert("Faltou exatamente " + b + " para passar");
+    }
+}
+
+let matematica = prompt("Digite aqui a sua nota em matematica");
+let portugues = prompt("Digite aqui a sua nota em Portugues");
+let ciencia = prompt("Digite aqui a sua nota em Ciencia");
+let redacao = prompt("Digite aqui a sua nota em Redação");
+
+let resultadoDaVerificacao = verificacaoDeDados (matematica,portugues,ciencia,redacao);
+
+if(resultadoDaVerificacao == matematica) {
+    alert("Por favor, digite numeros maior que 0 e menor ou igual a 10");
+} else {
+
+let matematicaComPeso = matematicaERedacaoPeso (matematica);
+let redacaoComPeso = matematicaERedacaoPeso (redacao);
+let portuguesComPeso = portuguesECienciasPeso (portugues);
+let cienciaComPeso = portuguesECienciasPeso (ciencia);
+
+let notaTotal = notaTotalPeso (matematicaComPeso,portuguesComPeso,redacaoComPeso,cienciaComPeso);
+
+resultado (notaTotal);
+
+}
